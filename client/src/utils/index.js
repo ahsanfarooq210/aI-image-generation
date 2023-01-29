7c606450cb6872368299813a02b55e367b375fd9
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2023-01-29 15:30:41 +0500

    application tutorial is now finalized
    finalized the app

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,8 +1,12 @@
 import {surpriseMePrompts} from '../constants'
-
+import FileSaver from 'file-saver'
 export function getRandomPrompt(prompt){
     const randomIndex=Math.floor(Math.random()*surpriseMePrompts.length)
     const randomprompt=surpriseMePrompts[randomIndex]
     if(randomprompt===prompt) return getRandomPrompt(prompt)
     return randomprompt
+}
+
+export async function downloadImage(_id,photo){
+    FileSaver.saveAs(photo,`download-${_id}.jpeg`)
 }
